79dd7d0b7e775ca2416bf65c005a09f01bff7e57
Vachan MN <vachanmn123@gmail.com>
2022-03-28 12:40:07 +0530

    basics are done!

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, flash
+import json
 
+with open('secrets.json') as json_file:
+    secrets = json.load(json_file)
 app = Flask(__name__)
+app.templates_auto_reload = True
+app.secret_key = secrets['secret_key']
+
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if request.method == 'POST':
+        file = request.files['picture']
+        flash(f'File {file.filename} uploaded successfully')
+        return redirect(url_for('success'))
+
+@app.route('/success')
+def success():
+    return render_template('success.html')
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0',port=8000)
+    app.run(host='0.0.0.0', port=8000, debug=True)
